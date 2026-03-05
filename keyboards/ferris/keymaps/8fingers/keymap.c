d2758e9f3a76286fe7821563726309100d4cc42c
Ramsesian <rowanrdrake@gmail.com>
2026-03-05 00:45:13 -0700

    Added double tap escape

--- keyboards/ferris/keymaps/8fingers/keymap.c
+++ keyboards/ferris/keymaps/8fingers/keymap.c
@@ -28,7 +28,8 @@ enum layer_names {
 enum combos {
     SD_SPACE,
     DF_ENTER,
-    KL_BKSP
+    KL_BKSP,
+    SV_SUPER
 };
 
 // define combo sequences here
@@ -40,13 +41,15 @@ check out the config for it as well
 const uint16_t PROGMEM enter_combo[] = {KC_S, KC_D, COMBO_END};
 const uint16_t PROGMEM space_combo[] = {KC_E, KC_D, COMBO_END};
 const uint16_t PROGMEM backspace_combo[] = {KC_I, KC_K, COMBO_END};
+const uint16_t PROGMEM super_combo[] = {KC_S, KC_C, COMBO_END};
 
 
 // when you hit a combo sequence you output X
 combo_t key_combos[] = {
     [SD_SPACE] = COMBO(space_combo, KC_SPACE),
     [DF_ENTER] = COMBO(enter_combo, KC_ENTER),
-    [KL_BKSP] = COMBO(backspace_combo, KC_BACKSPACE)
+    [KL_BKSP] = COMBO(backspace_combo, KC_BACKSPACE),
+    [SV_SUPER] = COMBO(super_combo, KC_LWIN),
 };
 //Pro Micro With The Bootloader ATmega32U4 5V/16MHz Module Controller Mega32U4 Leonardo For Arduino
 
@@ -63,15 +66,24 @@ tap_dance_action_t tap_dance_actions[] = {
 in your keyboard you use TD(TAP_DANCE_NAME)
 */
 
+enum tap_dance_actions {
+    QESC
+};
+
+tap_dance_action_t tap_dance_actions[] = {
+    [QESC] = ACTION_TAP_DANCE_DOUBLE(KC_Q, KC_ESCAPE)
+};
+
+
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_TALON] = LAYOUT_split_3x5_2(
     //,--------+--------+--------+--------+--------.              ,--------+--------+--------+--------+--------.
-        XXXXXXX,  DELEFT, DERIGHT, XXXXXXX, XXXXXXX,                XXXXXXX, XXXXXXX, XXXXXXX, KC_F18, XXXXXXX,
+        XXXXXXX,  DELEFT, DERIGHT, XXXXXXX, XXXXXXX,                XXXXXXX, KC_KB_VOLUME_DOWN, KC_KB_VOLUME_UP, KC_F18, XXXXXXX,
     //|--------+--------+--------+--------+--------|              |--------+--------+--------+--------+--------|
-      A(KC_TAB), WINLEFT, WINRGHT,  KC_F13, XXXXXXX,                XXXXXXX, MS_WHLU, MS_WHLD, KC_ENTER, KC_F17, //MS WHLD/WHLU refer to mouse wheel down/up respectively
+      A(KC_TAB), WINLEFT, WINRGHT,  KC_F13, XXXXXXX,                XXXXXXX, KC_ENTER,MS_WHLD, MS_WHLU,  KC_F17, //MS WHLD/WHLU refer to mouse wheel down/up respectively
     //|--------+--------+--------+--------+--------|              |--------+--------+--------+--------+--------|
       KC_LALT, S(KC_TAB),  KC_TAB,  XXXXXXX, XXXXXXX,                XXXXXXX, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT,
     //|--------+--------+--------+--------+--------|              |--------+--------+--------+--------+--------|
@@ -81,11 +93,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_QWERTY] = LAYOUT_split_3x5_2(
     //,--------+------------+------------+---------+---------.              ,--------+--------+---------------+--------------+---------------.
-           KC_Q,        KC_W,        KC_E,     KC_R,     KC_T,                   KC_Y,    KC_U,           KC_I,          KC_O,           KC_P,
+       TD(QESC),        KC_W,        KC_E,     KC_R,     KC_T,                   KC_Y,    KC_U,           KC_I,          KC_O,           KC_P,
     //|--------+------------+------------+---------+---------|              |--------+--------+---------------+--------------+---------------|
     SFT_T(KC_A),        KC_S,        KC_D,     KC_F,     KC_G,                   KC_H,    KC_J,           KC_K,          KC_L, SFT_T(KC_SCLN),
     //|--------+------------+------------+---------+---------|              |--------+--------+---------------+--------------+---------------|
-           KC_Z, CTL_T(KC_X), ALT_T(KC_C),     KC_V,     KC_B,                   KC_N,    KC_M, ALT_T(KC_COMM), CTL_T(KC_DOT),       KC_QUOTE,
+           KC_B, CTL_T(KC_Z), ALT_T(KC_X),     KC_C,     KC_V,                   KC_N,    KC_M, ALT_T(KC_COMM), CTL_T(KC_DOT),       KC_QUOTE,
     //|--------+------------+------------+---------+---------|              |--------+--------+---------------+--------------+---------------|
                                            KC_SPACE, KC_ENTER,                _______, KC_BACKSPACE
     //                                   `-------------------'              `-----------------'
